bdd78057ff4ad7b73f07ea75be6e135804fd9b03
Chen Liao <chenliao@vpnduopool-10-251-165-212.near.illinois.edu>
2024-04-05 00:29:10 +0800

    rename folder

--- Fig2|EDFig1b_2_3|TableS2/FEAST/run_feast.r
+++
@@ -1,7 +0,0 @@
-library(FEAST)
-
-metadata <- Load_metadata(metadata_path = "metadata_DSS_2E.txt")
-otus <- Load_CountMatrix(CountMatrix_path = "asv_DSS_2E.txt")
-
-FEAST_output <- FEAST(C = otus, metadata = metadata, different_sources_flag = 0, dir_path = "./", outfile="DSS_2E")
-
